7dee77bad9d2d8b4e497731e6a9c80b0a6e02bdc
Anna Cervo <anna@macbook-pro-di-anna-2.local>
2017-12-15 12:16:51 +0100

    Initial Commit

---
+++ Foods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foods
+//
+//  Created by Anna Grazia Cervo on 15/12/17.
+//  Copyright © 2017 Anna Grazia Cervo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
